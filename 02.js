9033cd10e9e4d774606c899ba41630e1fe0d2ee2
Sidd_0710 <siddheshwarchirkute2003@gmail.com>
2025-02-08 12:06:22 +0530

    Print the Number using  Using loop

---
+++ 02.js
@@ -0,0 +1,23 @@
+// Print the Number from 1 to 5 to the console Using loop
+
+console.log("Using A for loop ");
+
+for (let i = 1; i <=5; i++) {
+    console.log(i); 
+}
+
+console.log("Using A while loop ");
+
+let i =1;
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+console.log("Using A Do-While loop ");
+
+let j=1;
+do {
+    console.log(j);
+    j++;
+} while (j<=5);
