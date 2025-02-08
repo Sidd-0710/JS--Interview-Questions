ac79846cafdf5bd54339064d733765f22f49e0b1
Sidd_0710 <siddheshwarchirkute2003@gmail.com>
2025-02-08 17:27:09 +0530

    Even OR Odd

---
+++ 06.js
@@ -0,0 +1,15 @@
+//Even Or Odd
+
+
+function checkEvenOrOdd (num){
+
+    if (num % 2 == 0) {
+        return"Even"; 
+    } else
+    {
+        return "Odd";
+    }
+
+}
+
+console.log(checkEvenOrOdd(121));
